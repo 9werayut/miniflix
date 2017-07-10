5662fbe458c74274423bd8eab33ef5894f6ac456
9werayut <werayut.ku@gmail.com>
2017-07-11 00:13:30 +0700

    init commit

---
+++ src/components/Callback.js
@@ -0,0 +1,18 @@
+import { React, Component } from 'react';
+import { setIdToken, setAccessToken } from '../utils/AuthService';
+
+class Callback extends Component {
+
+    componentDidMount() {
+        setAccessToken();
+        setIdToken();
+        window.location.href = '/';
+    }
+    
+
+    render() {
+        return null;
+    }
+}
+
+export default Callback;
